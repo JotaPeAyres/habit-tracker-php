70f5f5d00c469386e26dd9c26aaad1f2501ebb43
JotaPe <joaopedroayres.dev@gmail.com>
2026-03-16 11:38:26 -0300

    feat: implement authentication functionality in LoginController and update login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,5 +3,38 @@
         <h1>
             Faça login
         </h1>
+
+        <section>
+            <form action="/login" method="POST">
+                @csrf
+
+                @error('email')
+                    <p class="text-red-500 text-xl mt-1">
+                        {{ $message }}
+                    </p>
+                @enderror
+
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="E-mail"
+                    class="bg-white p-2 border-2"
+                >
+
+                <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="********"
+                    class="bg-white p-2 border-2"
+                >
+
+                <button
+                    type="submit"
+                    class="bg-white p-2 border-2 "
+                >
+                    Entrar
+                </button>
+            </form>
+        </section>
     </main>
 </x-layout>
