d2850ae341aeaa01380e39424ef79d3044351726
JotaPe <joaopedroayres.dev@gmail.com>
2026-03-16 15:51:09 -0300

    refactor: clean up route comments and formatting in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,12 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-//SITE
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
-
-//LOGIN
 Route::get('/login', [LoginController::class, 'index'])->name('site.login');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('auth.login');
 
-
 //AUTH
 Route::middleware('auth')->group(function () {
-    //DASHBOARD
     Route::get('/dashboard', [SiteController::class, 'dashboard'])->name('site.dashboard');
     Route::post('/logout', [LoginController::class, 'logout'])->name('auth.logout ');
 });
