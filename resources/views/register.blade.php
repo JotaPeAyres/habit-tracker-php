ba4dff389113e7afab8bbc4503da423b01f3c5da
JotaPe <joaopedroayres.dev@gmail.com>
2026-03-17 11:45:25 -0300

    feat: implement registration functionality with RegisterController and views

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,20 +1,33 @@
 <x-layout>
     <main class="py-10">
-        <h1>
-            Faça login
-        </h1>
-
         <section class="bg-white max-w-[600px] mx-auto p-10 pb-6 border-2 mt-4">
             <h1 class="font-bold text-3xl">
-                Faça Login!
+                Registre-se!
             </h1>
 
             <p>
-                Insira seus dados para acessar
+                Preencha as informações para cadastrar seus hábitos 
             </p>
 
-            <form action="{{ route('auth.login') }}" method="POST" class="flex flex-col">
+            <form action="{{ route('auth.register')}}" method="POST" class="flex flex-col">
                 @csrf
+
+                <div class="flex flex-col gap-2 mb-4">
+                    <label for="name">
+                        Nome:
+                    </label>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Seu nome"
+                        class="bg-white p-2 border-2 @error('name') border-red-500 @enderror"
+                    >
+                    @error('name')
+                        <p class="text-red-500 text-sm">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
 
                 <div class="flex flex-col gap-2 mb-4">
                     <label for="email">
@@ -50,18 +63,35 @@
                     @enderror
                 </div>
 
+                <div class="flex flex-col gap-2 mb-4">
+                    <label for="password_confirmation">
+                        Repita sua senha:
+                    </label>
+                    <input 
+                        type="password" 
+                        name="password_confirmation" 
+                        placeholder="********"
+                        class="bg-white p-2 border-2 @error('password') border-red-500 @enderror"
+                    >
+                     @error('password')
+                        <p class="text-red-500 text-sm">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
                 <button
                     type="submit"
                     class="bg-white p-2 border-2 "
                 >
-                    Entrar
+                    Cadastrar
                 </button>
             </form>
 
             <p class="text-center mt-2">
-                Ainda não tem uma conta?
-                <a href="{{ route('site.register')}}" class="underline hover:opacity-50 transition">
-                    Registre-se
+                Já tem uma conta?
+                <a href="{{ route('site.login')}}" class="underline hover:opacity-50 transition">
+                    Faça login!
                 </a>
             </p>
         </section>
