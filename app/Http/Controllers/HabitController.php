6d7d8b67be156a7e7d3feeb172200e567f87103e
JotaPe <joaopedroayres.dev@gmail.com>
2026-03-20 17:04:55 -0300

    feat: add habit deletion functionality with confirmation in dashboard

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -59,6 +59,15 @@ class HabitController extends Controller
      */
     public function destroy(Habit $habit)
     {
-        //
+        // dd('habit'. $habit->user_id, auth()->user()->id);
+
+        if($habit->user_id != auth()->user()->id)
+            abort(403, 'Esse hábito não é seu!');
+
+        $habit->delete();
+
+        return redirect()
+            ->route('site.dashboard')
+            ->with('success', 'Hábito removido com sucesso!');
     }
 }
