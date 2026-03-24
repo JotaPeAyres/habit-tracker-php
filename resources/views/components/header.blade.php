21158838adfa36d1f380a50d6a93cac74f9f3080
JotaPe <joaopedroayres.dev@gmail.com>
2026-03-24 18:37:09 -0300

    feat: add contribution component and enhance dashboard layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,38 +1,41 @@
-<header class="bg-white border-b-2 flex items-center justify-between p-4">
-    {{-- LOGO --}}
-    <div class="flex items-center gap-2">
-        <a href="{{ route('habits.index') }}" class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
-            HT
-        </a>
-        <p>
-            Habit Tracker
-        </p>
-    </div>
-    <div>
-        @auth
-            <form 
-                class="inline" 
-                action="{{ route('auth.logout ') }}" 
-                method="POST"
-            >
-                @csrf
-
-                <button type="submit" class="habit-shadow-lg habit-btn p-2">
-                    Sair
-                </button>
-            </form>
-        @endauth
-
-        @guest
-        <div class="flex gap-2">
-            <a href="{{ route('site.register') }}" class="p-2 border-2 habit-shadow-lg habit-btn">
-                Cadastrar
+<header class="bg-white border-b-2">
+    <div class="max-w-7xl mx-auto flex items-center justify-between p-4">
+        {{-- LOGO --}}
+        <div class="flex items-center gap-2">
+            <a href="{{ route('habits.index') }}" class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
+                HT
             </a>
-
-            <a href="{{ route('auth.login') }}" class="p-2 border-2 habit-shadow-lg bg-habit-orange habit-btn">
-                Logar
-            </a>
+            <p>
+                Habit Tracker
+            </p>
         </div>
-        @endguest
+
+        <div>
+            @auth
+                <form 
+                    class="inline" 
+                    action="{{ route('auth.logout ') }}" 
+                    method="POST"
+                >
+                    @csrf
+
+                    <button type="submit" class="habit-shadow-lg habit-btn p-2">
+                        Sair
+                    </button>
+                </form>
+            @endauth
+
+            @guest
+            <div class="flex gap-2">
+                <a href="{{ route('site.register') }}" class="p-2 border-2 habit-shadow-lg habit-btn">
+                    Cadastrar
+                </a>
+
+                <a href="{{ route('auth.login') }}" class="p-2 border-2 habit-shadow-lg bg-habit-orange habit-btn">
+                    Logar
+                </a>
+            </div>
+            @endguest
+        </div>
     </div>
 </header>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,41 +1,65 @@
 <x-layout>
-  <main class="max-w-5xl mx-auto py-10 px-4 min-h-[80vh] w-full">
+  <main class="max-w-7xl mx-auto py-10 min-h-[calc(100vh-160px)] px-4">
+
     <x-navbar />
 
-    <div class="flex flex-col gap-4 items-start">
+    @session('success')
+      <div class="flex">
+        <p class="bg-green-100 border-2 border-green-400 text-green-700 p-3 mb-4">
+          {{ session('success') }}
+        </p>
+      </div>
+    @endsession
 
-      <ul class="flex flex-col gap-2 w-full">
-        @forelse($habits as $item)
-          <li class="habit-shadow-lg p-2 bg-[#FFDAAC]">
-            <form
-              action="{{ route('habits.toggle', $item->id) }}"
-              method="POST"
-              class="flex gap-2 items-center"
-              id="form-{{ $item->id }}"
-            >
-              @csrf
-
-              <input
-                type="checkbox"
-                class="w-5 h-5" {{ $item->is_completed ? 'checked' : '' }}
-                {{ $item->wasCompletedToday() ? 'checked' : '' }}
-                onchange="document.getElementById('form-{{ $item->id }}').submit()"
-              />
-              <p class="font-bold text-lg">
-                {{ $item->name }}
-              </p>
-            </form>
-          </li>
-        @empty
-          <p>
-            Ainda não tem nenhuma hábito cadastrado
+    <div>
+      @forelse($habits as $habit)
+        <x-contribution :habit="$habit" />
+      @empty
+        <div>
+          <p class="text-black">
+            Nenhum hábito para exibir histórico.
           </p>
-        @endforelse
-      </ul>
-
-      <a href="{{ route('habits.create') }}" class="p-2 habit-shadow-lg bg-habit-orange habit-btn">
-        + Adicionar
-      </a>
+          <a href="{{ route('habits.create') }}" class="underline ">
+            Crie um novo hábito
+          </a>
+        </div>
+      @endforelse
     </div>
+
+    <h2 class="text-lg mt-8 mb-2">
+      {{ date('d/m/Y') }}
+    </h2>
+
+    <ul class="flex flex-col gap-2">
+      @forelse($habits as $item)
+        <li class="habit-shadow-lg p-2 bg-[#FFDAAC]">
+          <form
+            action="{{ route('habits.toggle', $item->id) }}"
+            method="POST"
+            class="flex gap-2 items-center"
+            id="form-{{ $item->id }}"
+          >
+            @csrf
+
+            <input
+              type="checkbox"
+              class="w-5 h-5" {{ $item->is_completed ? 'checked' : '' }}
+              {{ $item->wasCompletedToday() ? 'checked' : '' }}
+              onchange="document.getElementById('form-{{ $item->id }}').submit()"
+            />
+            <p class="font-bold text-lg">
+              {{ $item->name }}
+            </p>
+          </form>
+        </li>
+      @empty
+        <p>
+          Ainda não tem nenhuma hábito cadastrado
+        </p>
+        <a href="{{ route('habits.create') }}" class="bg-white p-2 border-2">
+          Cadastre um novo hábito agora
+        </a>
+      @endforelse
+    </ul>
   </main>
 </x-layout>
